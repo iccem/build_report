5545371442898988599c257a729e0cb497956e75
Lilia Filippova <delo5070@gmail.com>
2024-01-25 08:57:29 +0400

    Build STG-tables.

---
+++ scr/create_file_links_list_from_db.py
@@ -0,0 +1,164 @@
+import sqlite3
+
+
+def create_STG_tables(company, date_start, date_end):
+    con = sqlite3.connect('scr/doc/online_adv.db')
+    cursor = con.cursor()
+
+    cursor.execute('DROP TABLE IF EXISTS STG_COMMON_CLICK_LINKS')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS STG_COMMON_CLICK_LINKS as
+        select * from (
+        select
+            ID
+            , дата
+            , клиент
+            , сайт as link
+            , 'сайт' as source
+            from adv_publications
+            where
+            клиент = ? 
+            and
+            сайт is not null
+        union
+        select
+            ID
+            , дата
+            , клиент
+            , VK1 as link
+            , 'VK 1' as source
+            from adv_publications
+            where
+            клиент = ?
+            and
+            VK1 is not null
+        union
+        select
+            ID
+            , дата
+            , клиент
+            , VK2 as link
+            , 'VK 2' as source
+            from adv_publications
+            where
+            клиент = ? 
+            and
+            VK2 is not null
+        union
+        select
+            ID
+            , дата
+            , клиент
+            , TG as link
+            , 'TG' as source
+            from adv_publications
+            where
+            клиент = ? 
+            and
+            TG is not null)
+        where дата >= datetime(?)
+        and   дата <= ? 
+    ''', [company, company, company, company, date_start, date_end])
+
+
+    cursor.execute('DROP TABLE IF EXISTS STG_TW_CLICK_LINKS')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS STG_TW_CLICK_LINKS as
+        select * from (
+        select
+            ID
+            , дата
+            , клиент
+            , TW as link
+            , 'TW' as source
+            from adv_publications
+            where
+            клиент = ? 
+            and
+            TW is not null)
+        where дата >= datetime(?)
+        and   дата <= ? 
+    ''', [company, date_start, date_end])
+
+
+    cursor.execute('DROP TABLE IF EXISTS STG_REPORT_LINKS')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS STG_REPORT_LINKS as
+        select * from (
+        select
+            ID
+            , дата
+            , клиент
+            , сайт as link
+            , 'сайт' as source
+            from adv_publications
+            where
+            клиент = ? 
+            and
+            сайт is not null
+        union
+        select
+            ID
+            , дата
+            , клиент
+            , VK1 as link
+            , 'VK 1' as source
+            from adv_publications
+            where
+            клиент = ?
+            and
+            VK1 is not null
+        union
+        select
+            ID
+            , дата
+            , клиент
+            , VK2 as link
+            , 'VK 2' as source
+            from adv_publications
+            where
+            клиент = ? 
+            and
+            VK2 is not null
+        union
+        select
+            ID
+            , дата
+            , клиент
+            , TG as link
+            , 'TG' as source
+            from adv_publications
+            where
+            клиент = ? 
+            and
+            TG is not null
+        union
+            select
+            ID
+            , дата
+            , клиент
+            , TW as link
+            , 'TW' as source
+            from adv_publications
+            where
+            клиент = ? 
+            and
+            TW is not null)
+        where дата >= datetime(?)
+        and   дата <= ? 
+    ''', [company, company, company, company, company, date_start, date_end])
+
+
+    # cursor.execute('''
+    #     select * from STG_REPORT_LINKS
+    #     where дата >= datetime(?)
+    #     and   дата <= ?
+    #     ''', [date_start, date_end])
+
+    # for row in cursor.fetchall():
+    #     # print(row)
+    #     report_links.append(row)
+    
--- scr/main.py
+++ scr/main.py
@@ -1,5 +1,6 @@
 import excel_to_db as excel_to_db
 import doc.paths as doc
+import create_file_links_list_from_db as cf
 
 
 if __name__=='__main__':
@@ -9,3 +10,12 @@ if __name__=='__main__':
     
     
     excel_to_db.load(file_excel_report)
+
+    #  Fill in the form current report.
+    company = doc.company 
+    report_month = doc.report_month
+    date_start = doc.date_start
+    date_end = doc.date_end
+
+    #  Create stg-tables.
+    cf.create_STG_tables(company, date_start, date_end)
