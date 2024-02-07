8f188cb5ea33e9b023d2539bd475257aac4f52d1
Lilia Filippova <delo5070@gmail.com>
2024-02-07 09:44:49 +0400

    fix make screen

--- make_screen.py
+++ make_screen.py
@@ -46,45 +46,19 @@ def _get_common_links(common_click_links):
 
     for row in cursor.fetchall():
         common_click_links.append(row)
+    return common_click_links
 
-
-def _get_common_tw_links(tw_click_links):
-    '''Получает список ссылок для твиттера из таблицы 
-    и генерирует названия файлов.
-    Возвращает список списков: имя файла и ссылка'''
-    con = sqlite3.connect('doc/online_adv.db')
-    cursor = con.cursor()
-    cursor.execute('''
-        select * from STG_TW_CLICK_LINKS
-        ''')
-
-    for row in cursor.fetchall():
-        tw_click_links.append(row)
 
 
 def _make_common_screen():
     common_click_links = []
     temp = {}
-    _get_common_links(common_click_links)
+    common_click_links =_get_common_links(common_click_links)
 
     for row in common_click_links:
         date = str(row[1]).replace(" 00:00:00", "")
         filename = str(row[0]) + '   ' + date + '   ' + row[2] + '   ' + row[4] + '.png'
         path = row[3].replace("vk.com", "m.vk.com")
-        temp[filename] = path
-    # print('*')
-    return temp
-
-
-def _make_tw_screen():
-    tw_click_links = []
-    temp = {}
-    _get_common_tw_links(tw_click_links)
-
-    for row in tw_click_links:
-        date = str(row[1]).replace(" 00:00:00", "")
-        filename = str(row[0]) + '   ' + date + '   ' + row[2] + '   ' + row[4] + '.png'
-        path = row[3]
         temp[filename] = path
     # print('*')
     return temp
@@ -102,15 +76,4 @@ def make_screen(path_for_screens):
     rows_common_screen = _make_common_screen()
     _get_selenium(rows_common_screen, path_for_screens, 5)
     
-    rows_tw_screen = _make_tw_screen()
-
-    if len(rows_tw_screen) > 0:
-        print('ВКЛЮЧИТЬ *** и НАЖАТЬ ENTER!')
-        print('Наблюдай! Надо закрывать всплывающие окна вручную!')
-        print('Примечание: Лантен работает, попробуй подождать')
-        T = input()
-
-        _get_selenium(rows_tw_screen, path_for_screens, 30)
-
-        print('МОЖНО ВЫКЛЮЧИТЬ!')
     
